a7d8ba8561af981c494e2dd31b2c51edcfb60c99
ZhaoDiDiDi <739333160@qq.com>
2021-02-26 11:24:56 +0800

    在yml filters增加了DedupeResponseHeader解决header重复导致跨域失败的问题

--- gulimall-gateway/src/main/java/com/atguigu/gulimall/gateway/config/GulimallCorsConfiguration.java
+++ gulimall-gateway/src/main/java/com/atguigu/gulimall/gateway/config/GulimallCorsConfiguration.java
@@ -2,30 +2,15 @@ package com.atguigu.gulimall.gateway.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.server.reactive.ServerHttpRequest;
-import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.reactive.CorsConfigurationSource;
-import org.springframework.web.cors.reactive.CorsUtils;
 import org.springframework.web.cors.reactive.CorsWebFilter;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.reactive.HiddenHttpMethodFilter;
-import org.springframework.web.reactive.config.WebFluxConfigurer;
-import org.springframework.web.server.ServerWebExchange;
-import org.springframework.web.server.WebFilter;
-import org.springframework.web.server.WebFilterChain;
-import org.springframework.web.util.pattern.PathPatternParser;
-import reactor.core.publisher.Mono;
 
+/**
+ * 网关跨域配置
+ */
 @Configuration
 public class GulimallCorsConfiguration{
-    @Bean
-    public CorsResponseHeaderFilter corsResponseHeaderFilter() {
-        return new CorsResponseHeaderFilter();
-    }
 
     @Bean
     public CorsWebFilter corsFilter() {
